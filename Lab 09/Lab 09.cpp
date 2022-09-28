260b15c2453d4d2668851f0130c135333a35ef59
Maksym Pervov <83817268+makspervov@users.noreply.github.com>
2022-09-28 23:38:49 +0200

    Add files via upload

---
+++ Lab 09/Lab 09.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using namespace std;
+struct element
+{
+	int number;
+	element* next;
+};
+
+void push_stos(element*& stos, int wartosc)
+{
+	element* el = new element;
+	el->number = wartosc;
+	el->next = stos;
+	stos = el;
+}
+
+void pop_stos(element*& stos)
+{
+	if (stos == nullptr)
+	{
+		cout << "Stos jest pusty!" << endl;
+	}
+	element* temp = stos;
+	stos = stos->next;
+	delete temp;
+}
+
+bool czyPusty_Stos(element* stos)
+{
+	if (stos == nullptr)
+	{
+		cout << "Stos jest pusty" << endl;
+		return true;
+	}
+	else
+	{
+		cout << "Stos cos zawiera" << endl;
+		return false;
+	}
+}
+
+int top_stos(element* stos)
+{
+	return stos->number;
+}
+
+int main()
+{
+	element* stos = nullptr;
+
+	czyPusty_Stos(stos);
+	push_stos(stos, 1);
+	cout << top_stos(stos) << endl;
+
+	czyPusty_Stos(stos);
+	push_stos(stos, 2);
+	cout << endl << top_stos(stos) << endl;
+
+	czyPusty_Stos(stos);
+	pop_stos(stos);
+	cout << endl << top_stos(stos) << endl;
+
+	czyPusty_Stos(stos);
+	pop_stos(stos);
+	czyPusty_Stos(stos);
+
+	system("pause");
+	return 0;
+}
---
+++ Lab 10/Lab 10.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct element
+{
+	char character;
+	element* next;
+};
+
+struct queue
+{
+	element* head;
+	element* tail;
+};
+
+void wczytaj()
+{
+	ifstream plik;
+	plik.open("lab10_tekst.txt");
+
+
+	plik.close();
+}
+
+void push(queue &q, char value)
+{
+	element* el = new element;
+	el->character = value;
+	el->next = nullptr;
+	if (q.tail != nullptr)
+	{
+		q.tail->next = el;
+	}
+	else
+	{
+		q.head = el;
+	}
+	q.tail = el;
+}
+
+void pop(queue& q)
+{
+	element* temp = q.head;
+	q.head = q.head->next;
+	delete temp;
+	if (q.head == nullptr)
+	{
+		q.tail = nullptr;
+	}
+}
+
+int czyPusty(queue q)
+{
+	if (q.head == nullptr)
+	{
+		cout << "Lista jest pusta";
+		return 1;
+	}
+	else
+	{
+		cout << "Lista nie jest pusta";
+		return 0;
+	}
+}
+
+int pierwszy(queue q)
+{
+	return q.head->character;
+}
+
+int main()
+{
+	int opcja;
+	cout << "Pervov Maksym, GL13" << endl << "Podstawy Algorytmiki" << endl << "Menu:" << endl;
+	cout << endl << "1. Sprawdzenie, czy kolejka jest pusta" << endl
+		<< "2. Dodanie elementu do kolejki" << endl
+		<< "3. Ususniecie elementu z kolejki" << endl
+		<< "4. Pobranie elementu z kolejki" << endl
+		<< "5. Usuniecie wszystkich elementow z kolejki" << endl
+		<< "6. Wczytanie do kolejki duzych liter alfabetu lacinskiego" << endl
+		<< "0. Wyjscie z programu" << endl;
+	cout << endl << "Wybierz funkcje: ";
+	cin >> opcja;
+	if (opcja == 1)
+	{
+		if (czyPusty == 0) cout << "Kolejka jest pusta" << endl << endl << endl;
+		else cout << "Kolejka nie jest pusta" << endl << endl << endl;
+		return main();
+	}
+	if (opcja == 2)
+	{
+		char klzn;
+		cout << "Dodaj kolejny znak: ";
+		cin >> klzn;
+		return main();
+	}
+	if (opcja == 3)
+	{
+
+		return main();
+	}
+	if (opcja == 4)
+	{
+		return main();
+	}
+	if (opcja == 5)
+	{
+		return main();
+	}
+	if (opcja == 6)
+	{
+		return main();
+	}
+	if (opcja == 0)
+	{
+		cout << "Do widzenia! :)" << endl;
+		system("pause");
+		return 0;
+	}
+	if (opcja != 0 && opcja != 1 && opcja != 2 && opcja != 3)
+	{
+		cout << "Niepoprawna liczba! Sprobuj ponownie :)" << endl << endl;
+		cout << endl << "Wybierz funkcje: ";
+		cin >> opcja;
+	}
+	queue q;
+	q.head = nullptr;
+	q.tail = nullptr;
+}
